bb6f58b57e9a89563ae675c4c73bbcc2a9d8f09e
Ervin Pepic <ervin.hack@gmail.com>
2018-03-31 15:34:25 +0200

    Keyoboard Events

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -7,20 +7,45 @@ import { Customer } from './../Customer';
     selector: 'sandbox',
     template: `
         <h1>Hello World</h1>
-        <button (click)="changeValue()">Change Value</button>
-        <div *ngIf="value">
-            <h1>{{ text }}</h1>
+        <div>
+            <input type="text" (keyup)="fireEvent($event)" placeholder="keyup"/>
         </div>
+        <div>
+        <input type="text" (keydown)="fireEvent($event)" placeholder="keydown"/>
+            </div>
+            <div>
+            <input type="text" (focus)="fireEvent($event)" placeholder="focus"/>
+        </div>
+        <div>
+        <input type="text" (blur)="fireEvent($event)" placeholder="blur"/>
+    </div>
+    <div>
+    <input type="text" (cut)="fireEvent($event)" placeholder="cut"/>
+    
+</div>
+<div>
+    <input type="text" (copy)="fireEvent($event)" placeholder="copy"/>
+    
+</div>
+
+<div>
+    <input type="text" (paste)="fireEvent($event)" placeholder="paste"/>
+    
+</div>
+<hr>
+<div>     <input type="text" (keyup)="changeText($event)" placeholder="changeText" value={{text}}/>      </div>
+
+<h2>{{ text }}</h2>
     `
 })
 
 export class SandboxComponent {
-    text: string = 'Hello World';
-    value: boolean = true;
-
-    changeValue(){
-        // this.value = false;
-        this.value = !this.value;
+    text: string = 'Hello';
+    fireEvent(e) {
+        console.log(e.type);
     }
 
+    changeText(e) {
+        this.text = e.target.value;
+    }
 }   
