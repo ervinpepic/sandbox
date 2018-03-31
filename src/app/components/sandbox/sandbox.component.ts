4c6140c4ca876df630772c1135e5b12a2b86175e
Ervin Pepic <ervin.hack@gmail.com>
2018-03-31 15:51:41 +0200

    Form submition

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -12,20 +12,17 @@ import { Customer } from './../Customer';
 
                 <h1 class="mb-5 mt-5">Hello World</h1>
             
-                <form>
+                <form (submit)="onSubmit()">
                     <div class="form-group">
                         <label>Name</label>
-                        <input type="text" [(ngModel)]="name" name="name" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label>Age</label>
-                        <input type="number" [(ngModel)]="age" name="age" class="form-control">
+                        <input type="text" class="form-control" [(ngModel)]="name" name="name">
                     </div>
                     <input type="submit" value="Submit" class="btn btn-success">
                 </form>   
                 <br>
-                <h5>Name: {{name}}</h5>
-                <h6>Age: {{age}}</h6>
+                <ul class="list-group">
+                    <li class="list-group-item" *ngFor="let user of users">{{user}}</li>
+                </ul>
             </div>    
         </div>
     </div>
@@ -34,5 +31,12 @@ import { Customer } from './../Customer';
 
 export class SandboxComponent {
     name: string = '';
-    age: number = 0;
+    users: string[] = ['Ervin', 'Emel', 'Erna', 'Hido'];
+    
+    onSubmit() {
+        this.users.push(this.name);
+        this.name = '';
+
+
+    }
 }   
